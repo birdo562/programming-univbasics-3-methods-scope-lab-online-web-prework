e0d71efb6bc61c4981c9d3b4eb0ab269fe1b8058
attentive-rubyist-8517 <robby.medina562@gmail.com>
2019-08-04 21:35:13 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,19 @@
+phrase = "It's-a me, Mario!"
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts phrase
+$status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+puts "It's-a me, Mario!"
 end
 
 def toadstool
-  puts status
+puts $status
 end
+
+Def link
+puts "It's Dangerous To GO Alone! Take This."
+end
+
+def any_phrase(phrase)
+phrase="Do A Barrel Roll!"
+end
+Any_phrase("Phrase")
+
